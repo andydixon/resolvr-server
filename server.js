c510981d5edbc8cbee4809b6edeec72617621356
Andy Dixon <andy@andydixon.uk>
2019-03-07 19:45:32 +0000

    Clean up repo, properly set up npm instead of bodging everything as normal :|
    now can start with $ npm start

--- server.js
+++ server.js
@@ -2,22 +2,22 @@
 
 /**
 
-    Resolvr DNS Server - Advert, Spyware and Malware DNS Level Blocker
-    Copyright (C) 2018- Andy Dixon
+ Resolvr DNS Server - Advert, Spyware and Malware DNS Level Blocker
+ Copyright (C) 2018- Andy Dixon
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-**/
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **/
 
 let memcached = require('memcached');
 let dns = require('native-dns');
@@ -36,9 +36,6 @@ let memcache = new memcached('localhost');
 let blacklist = staticZones['blacklist'];
 var io = require('socket.io').listen(61327);
 
-console.log('A R T E M I S  -  DNS Resolver Engine for cesolvr.cc');
-console.log('Version 1.0  (c) 2018-2019 Andy Dixon');
-
 function proxy(question, response, cb) {
     var request = dns.Request({
         question: question, // forwarding the question
@@ -49,13 +46,13 @@ function proxy(question, response, cb) {
     request.on('message', (err, msg) => {
         let ttl = 60;
         //msg.answer.forEach(a => response.answer.push(a));
-        msg.answer.forEach(function(record) {
+        msg.answer.forEach(function (record) {
             response.answer.push(record);
             ttl = record.ttl;
         });
-        memcache.set(question.name + question.type, response.answer, ttl, function(err, result) {
+        memcache.set(question.name + question.type, response.answer, ttl, function (err, result) {
             if (err) console.error(err);
-        }); // @fixme: 60 seconds needs to be replaced by the TTL
+        });
     });
 
     request.on('end', cb);
@@ -75,7 +72,7 @@ function handleRequest(request, response) {
     let f = [];
 
     request.question.forEach(question => {
-        memcache.get(question.name + question.type, function(err, data) {
+        memcache.get(question.name + question.type, function (err, data) {
             if (data) {
                 broadcast.note = "cached";
                 broadcast.status = 'primary';
@@ -87,7 +84,7 @@ function handleRequest(request, response) {
                     //so lets not send anything for a DNSFAIL.
                     //The comment block below shows how to craft a record
                     /**
-                      try{
+                     try{
                             entry[0].records.forEach(record => {
                             record.name = question.name;
                             record.ttl = record.ttl || 1800;
@@ -99,7 +96,7 @@ function handleRequest(request, response) {
                                     broadcast.note = "Exception AC1";
                                     broadcast.status = 'danger';
                             }
-                                    **/
+                     **/
                     broadcast.note = "Blacklisted";
                     broadcast.status = 'warning';
                 } else {
@@ -108,7 +105,7 @@ function handleRequest(request, response) {
             }
             io.emit("resolvr_monitor", broadcast);
             io.emit(generateHash(request.address.address), broadcast);
-            async.parallel(f, function() {
+            async.parallel(f, function () {
                 response.send();
             });
         });
@@ -121,7 +118,6 @@ function generateHash(ipAddress) {
 }
 
 
-
 server.on('request', handleRequest);
 
 server.on('listening', () => console.log('server listening on', server.address()));
@@ -130,7 +126,7 @@ server.on('error', (err, buff, req, res) => console.error(err.stack));
 server.on('socketError', (err, socket) => console.error(err));
 
 // For individual user monitoring, send the sha256 of their handle so they can subscribve
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
     var ipAddress = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address.address;
     socket.emit('handler', generateHash(ipAddress));
 });
